9d6731d8050faf5df66c71ac530451717b870fe3
rishimalve <rishi.malve@gmail.com>
2019-12-08 01:23:45 -0500

    added code for the rate of sarcasm over the years

--- analysis.py
+++ analysis.py
@@ -3,21 +3,19 @@ import numpy as np
 import pandas as pd
 import plotly.offline as py
 import plotly.graph_objs as go
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 
 train = pd.read_csv(
-    "/Users/rishimalve/Documents/Masters/Sem-3/CS 657/final_project/data/train-balanced-sarcasm.csv")
+    "/Users/rishimalve/Documents/Masters/Sem-3/CS 657/train-balanced-sarcasm.csv")
 
-## label count ##
-cnt_srs = train['label'].value_counts()
-trace = go.Bar(
-    x=cnt_srs.index,
-    y=cnt_srs.values,
-    marker=dict(
-        color=cnt_srs.values,
-        colorscale='Picnic',
-        reversescale=True
-    ),
-)
+train.dropna(subset=['comment'], inplace=True)
+
+################################ label count ################################
+
+label_count = train['label'].value_counts()
+trace = go.Bar(x=label_count.index, y=label_count.values, marker=dict(
+    color=label_count.values, colorscale='RdBu', reversescale=True),)
 
 layout = go.Layout(
     title='Label Count',
@@ -28,17 +26,31 @@ data = [trace]
 fig = go.Figure(data=data, layout=layout)
 py.iplot(fig, filename="TargetCount")
 
-## label distribution ##
-labels = (np.array(cnt_srs.index))
-sizes = (np.array((cnt_srs / cnt_srs.sum())*100))
+
+################################ label distribution ################################
+
+labels = (np.array(label_count.index))
+sizes = (np.array((label_count / label_count.sum())*100))
 
 trace = go.Pie(labels=labels, values=sizes)
-layout = go.Layout(
-    title='Label Distribution',
-    font=dict(size=18),
-    width=800,
-    height=600,
-)
+layout = go.Layout(title='Label Distribution',
+                   font=dict(size=18), width=800, height=600,)
 data = [trace]
 fig = go.Figure(data=data, layout=layout)
 py.iplot(fig, filename="usertype")
+
+
+################################ Rate of Sarcasm ################################
+
+df = train
+df['date'] = pd.to_datetime(df['date'], yearfirst=True)
+df['year'] = df['date'].apply(lambda d: d.year)
+df.drop(['date', 'created_utc'], axis=1, inplace=True)
+
+comments_by_year = df.groupby('year')['label'].agg([np.sum,np.mean])
+
+plt.figure(figsize=(8,6))
+comments_by_year['mean'].plot(kind='line')
+plt.ylabel('Mean Sarcasm')
+plt.title('Rate of Sarcasm on Reddit')
+plt.show()
