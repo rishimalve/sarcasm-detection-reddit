893f4a9cccb04e28ca897c75b58d6c70c8040f26
rishimalve <rishi.malve@gmail.com>
2019-12-08 22:58:48 -0500

    added word clouds

--- analysis.py
+++ analysis.py
@@ -8,7 +8,7 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import seaborn as sns
 from collections import defaultdict
-from wordcloud import WordCloud, STOPWORDS
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
 train = pd.read_csv(
     "/Users/rishimalve/Documents/Masters/Sem-3/CS 657/train-balanced-sarcasm.csv")
@@ -95,24 +95,29 @@ train1_df = train[train["label"] == 1]
 train0_df = train[train["label"] == 0]
 
 ## custom function for ngram generation ##
-def generate_ngrams(text, n_gram=1):
+
+
+def generate_ngrams(text, n_gram=2):
     token = [token for token in text.lower().split(
         " ") if token != "" if token not in STOPWORDS]
     ngrams = zip(*[token[i:] for i in range(n_gram)])
     return [" ".join(ngram) for ngram in ngrams]
 
 ## custom function for horizontal bar chart ##
+
+
 def horizontal_bar_chart(df, color):
     trace = go.Bar(
         y=df["word"].values[::-1],
         x=df["wordcount"].values[::-1],
         showlegend=False,
-        orientation = 'h',
+        orientation='h',
         marker=dict(
             color=color,
         ),
     )
     return trace
+
 
 ## Get the bar chart from neutral text ##
 freq_dict = defaultdict(int)
@@ -134,9 +139,52 @@ trace1 = horizontal_bar_chart(fd_sorted.head(50), 'blue')
 
 # Creating two subplots
 fig = tls.make_subplots(rows=1, cols=2, vertical_spacing=0.04,
-                          subplot_titles=["Frequent words of neutral text", 
-                                          "Frequent words of sarcasm text"])
+                        subplot_titles=["Frequent words of neutral text",
+                                        "Frequent words of sarcasm text"])
 fig.append_trace(trace0, 1, 1)
 fig.append_trace(trace1, 1, 2)
-fig['layout'].update(height=1200, width=900, paper_bgcolor='rgb(233,233,233)', title="Word Count Plots")
+fig['layout'].update(height=1200, width=900,
+                     paper_bgcolor='rgb(233,233,233)', title="Word Count Plots")
 py.iplot(fig, filename='word-plots')
+
+
+################################ word cloud for sarcastic words ################################
+
+def plot_wordcloud(text, mask=None, max_words=200, max_font_size=100, figure_size=(24.0, 16.0), title=None, title_size=40, image_color=False):
+    stopwords = set(STOPWORDS)
+    more_stopwords = {'one', 'br', 'Po', 'th', 'sayi', 'fo', 'Unknown'}
+    stopwords = stopwords.union(more_stopwords)
+
+    wordcloud = WordCloud(background_color='black', stopwords=stopwords, max_words=max_words,
+                          max_font_size=max_font_size, random_state=42, width=800, height=400, mask=mask)
+    wordcloud.generate(str(text))
+
+    plt.figure(figsize=figure_size)
+    if image_color:
+        image_colors = ImageColorGenerator(mask)
+        plt.imshow(wordcloud.recolor(color_func=image_colors),
+                   interpolation="bilinear")
+        plt.title(title, fontdict={'size': title_size,
+                                   'verticalalignment': 'bottom'})
+    else:
+        plt.imshow(wordcloud)
+        plt.title(title, fontdict={
+                  'size': title_size, 'color': 'black', 'verticalalignment': 'bottom'})
+    plt.axis('off')
+    plt.tight_layout()
+    plt.show()
+
+
+plot_wordcloud(train1_df["comment"], title="Word Cloud of Comment")
+
+
+sarcastic_comments = str(train[train['label'] == 1]['comment'])
+plt.figure(figsize=(12, 12))
+word_cloud = WordCloud(stopwords=STOPWORDS)
+word_cloud.generate(sarcastic_comments)
+plt.imshow(word_cloud)
+
+
+################################ most sarcastic users ################################
+
+train.groupby('author')['label'].agg([np.sum,np.mean,np.size]).sort_values(by='sum',ascending=False).head(5)
